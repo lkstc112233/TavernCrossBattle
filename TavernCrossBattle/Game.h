7493f3bfa912edad60d320b4a00e9d9d5b9c2469
Photon cat <lkstc112233@users.noreply.github.com>
2020-05-24 23:39:23 -0700

    Game::Attack is now private.

--- TavernCrossBattle/Game.h
+++ TavernCrossBattle/Game.h
@@ -7,9 +7,11 @@ namespace battle {
 	public:
 		void Initialize(const Player& player1, const Player& player2);
 		void LoopStep();
-		void Attack(Minion *minion1, Minion *minion2);
 		const Board& GetBoardState();
 	private:
 		Board board_;
+
+		// minion1 attacks minion2.
+		void Attack(Minion *minion1, Minion *minion2);
 	};
 }
