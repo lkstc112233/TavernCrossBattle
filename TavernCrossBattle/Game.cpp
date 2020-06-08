2088a5ccb401f08fa37c9c07b62d23a37a40eba8
Photon cat <lkstc112233@users.noreply.github.com>
2020-06-08 00:27:47 -0700

    Implemented attacker/defender determination.

--- TavernCrossBattle/Game.cpp
+++ TavernCrossBattle/Game.cpp
@@ -1,6 +1,25 @@
 #include "Game.h"
 
+#include <random>
+
 namespace battle {
+	namespace {
+		std::default_random_engine random_generator;
+		Minion* DetermineAttacker(Player* player) {
+			for (int i = 0; i < player->minions_size(); ++i) {
+				if (player->minions(i).attacks_next()) {
+					return player->mutable_minions(i);
+				}
+			}
+			return nullptr;
+		}
+		Minion* DetermineDefender(Player* player) {
+			int minions = player->minions_size();
+			std::uniform_int_distribution<int> distribution(0, minions - 1);
+			return player->mutable_minions(distribution(random_generator));
+		}
+	}
+
 	void Game::Initialize(const Player& player1, const Player& player2) {
 		board_.clear_player1();
 		*board_.mutable_player1() = player1;
