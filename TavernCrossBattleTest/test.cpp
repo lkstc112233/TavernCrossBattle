ab3c553dd45bb472bff4497408dcf30a38862c22
Photon cat <lkstc112233@users.noreply.github.com>
2020-06-08 23:48:55 -0700

    Added a few test to make sure the library import is succeed.

--- TavernCrossBattleTest/test.cpp
+++ TavernCrossBattleTest/test.cpp
@@ -1,6 +1,19 @@
 #include "pch.h"
 
+#include "../TavernCrossBattle/protocpp/Minion.pb.h"
+#include "protocol-buffer-matchers.h"
+using nucleus::EqualsProto;
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
+}
+
+TEST(GMockIntegrationTest, TestName) {
+    EXPECT_THAT(true, true);
+}
+
+TEST(GMockIntegrationTest, ProtoTest) {
+    battle::Minion minion;
+
+    EXPECT_THAT(minion, EqualsProto(minion));
 }
