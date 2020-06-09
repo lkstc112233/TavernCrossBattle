9a6e8b1a235bf960a676fccaed258e412b1201d9
Photon cat <lkstc112233@users.noreply.github.com>
2020-06-09 00:48:43 -0700

    Added some random utilities.

--- TavernCrossBattle/Game.cpp
+++ TavernCrossBattle/Game.cpp
@@ -4,7 +4,16 @@
 
 namespace battle {
 	namespace {
-		std::default_random_engine random_generator;
+		std::random_device rd;
+		std::default_random_engine random_generator(rd());
+		std::uniform_int_distribution<> bool_distribution(0, 1);
+		int RandomInt(int min, int max) {
+			std::uniform_int_distribution<int> distribution(min, max);
+			return distribution(random_generator);
+		}
+		bool RandomBool() {
+			return bool_distribution(random_generator);
+		}
 		Minion* DetermineAttacker(Player* player) {
 			for (int i = 0; i < player->minions_size(); ++i) {
 				if (player->minions(i).attacks_next()) {
@@ -13,22 +22,48 @@ namespace battle {
 			}
 			return nullptr;
 		}
+		void DetermineNextAttacker(Player* player) {
+			// TODO: Implement this..
+			if (player->minions_size()) {
+				player->mutable_minions(0)->set_attacks_next(true);
+				for (int i = 1; i < player->minions_size(); ++i) {
+					player->mutable_minions(i)->set_attacks_next(false);
+				}
+			}
+		}
 		Minion* DetermineDefender(Player* player) {
 			int minions = player->minions_size();
-			std::uniform_int_distribution<int> distribution(0, minions - 1);
-			return player->mutable_minions(distribution(random_generator));
+			return player->mutable_minions(RandomInt(0, minions - 1));
+		}
+	}
+	namespace internal_testing {
+		void SetGeneratorSeed(int seed) {
+			random_generator.seed(seed);
 		}
 	}
 
 	void Game::Initialize(const Player& player1, const Player& player2) {
 		board_.clear_player1();
 		*board_.mutable_player1() = player1;
+		DetermineNextAttacker(board_.mutable_player1());
 		board_.clear_player2();
 		*board_.mutable_player2() = player2;
+		DetermineNextAttacker(board_.mutable_player2());
+		if (RandomBool()) {
+			board_.mutable_player1()->set_attacks_next(true);
+			board_.mutable_player2()->set_attacks_next(false);
+		} else {
+			board_.mutable_player1()->set_attacks_next(false);
+			board_.mutable_player2()->set_attacks_next(true);
+		}
 	}
 
 	void Game::LoopStep() {
-
+		Player* attackingPlayer = board_.player1().attacks_next() ? board_.mutable_player1() : board_.mutable_player2();
+		Player* defendingPlayer = board_.player1().attacks_next() ? board_.mutable_player2() : board_.mutable_player1();
+		Minion* attacker = DetermineAttacker(attackingPlayer);
+		Minion* defender = DetermineDefender(defendingPlayer);
+		Attack(attacker, defender);
 	}
 	void Game::Attack(Minion* attacker, Minion* defender) {
 		attacker->set_life_current(attacker->life_current() - defender->power());
--- TavernCrossBattleTest/GameTest.cpp
+++ TavernCrossBattleTest/GameTest.cpp
@@ -8,9 +8,19 @@
 
 using nucleus::EqualsProto;
 
+namespace battle {
+	namespace internal_testing {
+		void SetGeneratorSeed(int seed);
+	}
+}
+
 class GameTest : public ::testing::Test {
 protected:
 	battle::Game game_;
+
+	void SetUp() override {
+		battle::internal_testing::SetGeneratorSeed(0);
+	}
 };
 
 TEST_F(GameTest, InitializesCorrectly) {
@@ -23,7 +33,6 @@ TEST_F(GameTest, InitializesCorrectly) {
 			power: 3
 			life_total: 3
 			life_current: 3
-			attacks_next: true
 		}
 	)", &player1);
 
@@ -33,7 +42,6 @@ TEST_F(GameTest, InitializesCorrectly) {
 			power: 2
 			life_total: 3
 			life_current: 3
-			attacks_next: true
 		}
 	)", &player2);
 
@@ -56,7 +64,54 @@ TEST_F(GameTest, InitializesCorrectly) {
 			life_current: 3
 			attacks_next: true
 		}
+		attacks_next: true
 	}
 	)"));
 }
 
+TEST_F(GameTest, LoopStepCorrectlyLoops) {
+	battle::Player player1;
+	battle::Player player2;
+
+	google::protobuf::TextFormat::ParseFromString(R"(
+		minions {
+			id: 1
+			power: 3
+			life_total: 3
+			life_current: 3
+		}
+	)", &player1);
+
+	google::protobuf::TextFormat::ParseFromString(R"(
+		minions {
+			id: 2
+			power: 2
+			life_total: 3
+			life_current: 3
+		}
+	)", &player2);
+
+	game_.Initialize(player1, player2);
+	game_.LoopStep();
+	EXPECT_THAT(game_.GetBoardState(), EqualsProto(R"(
+	player1 {
+		minions {
+			id: 1
+			power: 3
+			life_total: 3
+			life_current: 1
+			attacks_next: true
+		}
+	}
+	player2 {
+		minions {
+			id: 2
+			power: 2
+			life_total: 3
+			life_current: 0
+			attacks_next: true
+		}
+		attacks_next: true
+	}
+	)"));
+}
